25ce5f8025c3fe3cdef77b60ae4d34f28d0fab71
Abd-ELrahmanHamza <a.m.hamza156@gmail.com>
2024-03-13 12:56:59 +0200

    add upload and download file

---
+++ data-keeper/client/client.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	pb "wireless_lab_1/gen"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("did not connect:", err)
+		return
+	}
+	defer conn.Close()
+	c := pb.NewMP4ServiceClient(conn)
+
+	// Upload file
+	uploadStream, err := c.UploadFile(context.Background())
+	if err != nil {
+		fmt.Println("Error opening upload stream:", err)
+		return
+	}
+	file, err := os.Open("example.mp4")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Send file content in chunks
+	chunkSize := 1024
+	buffer := make([]byte, chunkSize)
+	for {
+		n, err := file.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error reading file:", err)
+			return
+		}
+		if err := uploadStream.Send(&pb.FileChunk{Chunk: buffer[:n]}); err != nil {
+			fmt.Println("Error sending chunk:", err)
+			return
+		}
+	}
+	uploadResponse, err := uploadStream.CloseAndRecv()
+	if err != nil {
+		fmt.Println("Error receiving upload response:", err)
+		return
+	}
+	// fmt.Println("Upload response:", string(uploadResponse.GetFileContent()))
+	fmt.Println("Upload response:", string(uploadResponse.String()[0:100]))
+
+	// Download file
+	downloadStream, err := c.DownloadFile(context.Background(), &pb.FileRequest{FileName: "example.mp4"})
+	if err != nil {
+		fmt.Println("Error calling DownloadFile:", err)
+		return
+	}
+	outputFile, err := os.Create("downloaded.mp4")
+	if err != nil {
+		fmt.Println("Error creating output file:", err)
+		return
+	}
+	defer outputFile.Close()
+
+	// Receive and write file content in chunks
+	for {
+		chunk, err := downloadStream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error receiving chunk:", err)
+			return
+		}
+		_, err = outputFile.Write(chunk.GetChunk())
+		if err != nil {
+			fmt.Println("Error writing chunk to file:", err)
+			return
+		}
+	}
+	fmt.Println("File downloaded successfully.")
+}
---
+++ data-keeper/go.mod
@@ -0,0 +1,16 @@
+module wireless_lab_1
+
+go 1.19
+
+require (
+	google.golang.org/grpc v1.62.0
+	google.golang.org/protobuf v1.33.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.4 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240123012728-ef4313101c80 // indirect
+)
