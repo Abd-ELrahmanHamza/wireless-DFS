040e8d638ae3742c155020960006571c17da8e62
Abd-ELrahmanHamza <a.m.hamza156@gmail.com>
2024-03-14 17:04:09 +0200

    separate upload and download ports

--- data-keeper/clientrpc.go/clientrpc.go
+++ data-keeper/clientrpc.go/clientrpc.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:5001", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:5002", grpc.WithInsecure())
 	if err != nil {
 		fmt.Println("did not connect:", err)
 		return
@@ -17,7 +17,7 @@ func main() {
 	c := pb.NewDataKeeperServiceClient(conn)
 
 	// Replicate file
-	replicateResponse, err := c.ReplicateFile(context.Background(), &pb.ReplicateRequest{FileName: "example.mp4", Port: "8080"})
+	replicateResponse, err := c.ReplicateFile(context.Background(), &pb.ReplicateRequest{FileName: "example.mp4", Port: "8081"})
 	if err != nil {
 		fmt.Println("Error calling ReplicateFile:", err)
 		return
