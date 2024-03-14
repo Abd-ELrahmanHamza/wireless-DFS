5eecaa8dd6a2fd730ebd2c16083687482f9ee92d
Abd-ELrahmanHamza <a.m.hamza156@gmail.com>
2024-03-14 23:35:52 +0200

    add localhost to port selection

--- data-keeper/utils.go
+++ data-keeper/utils.go
@@ -6,27 +6,27 @@ import (
 )
 
 func findThreeAvailablePorts(startPort, endPort int) ([]int, error) {
-    var availablePorts []int
+	var availablePorts []int
 
-    for port := startPort; port <= endPort; port++ {
-        addr := fmt.Sprintf(":%d", port)
-        listener, err := net.Listen("tcp", addr)
-        if err != nil {
-            // Port is not available, continue to the next port
-            continue
-        }
-        // Port is available, close the listener and add it to the list of available ports
-        listener.Close()
-        availablePorts = append(availablePorts, port)
-        // Stop iterating if three available ports are found
-        if len(availablePorts) == 3 {
-            break
-        }
-    }
+	for port := startPort; port <= endPort; port++ {
+		addr := fmt.Sprintf("localhost:%d", port)
+		listener, err := net.Listen("tcp", addr)
+		if err != nil {
+			// Port is not available, continue to the next port
+			continue
+		}
+		// Port is available, close the listener and add it to the list of available ports
+		listener.Close()
+		availablePorts = append(availablePorts, port)
+		// Stop iterating if three available ports are found
+		if len(availablePorts) == 3 {
+			break
+		}
+	}
 
-    if len(availablePorts) == 0 {
-        return nil, fmt.Errorf("no available ports found in the range %d-%d", startPort, endPort)
-    }
+	if len(availablePorts) == 0 {
+		return nil, fmt.Errorf("no available ports found in the range %d-%d", startPort, endPort)
+	}
 
-    return availablePorts, nil
+	return availablePorts, nil
 }
